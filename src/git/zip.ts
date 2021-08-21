31eca774fdfe4ec139d5228ee85c8d35f5d30dee
Garfield550 <openpgp@550.moe>
2021-08-21 20:26:40 +0000

    feat: createarchive service

---
+++ src/git/zip.ts
@@ -0,0 +1,42 @@
+import SevenZip from 'node-7z';
+import { config } from 'src/configs';
+import { logger } from 'src/logger';
+import which from 'which';
+
+export class ZipArchiveService {
+  constructor() {
+    const bin = config.get<string>('sevenZip.binName');
+
+    if (!bin)
+      throw new Error('ZipArchiveService: can not get bin name in config');
+
+    const path = which.sync(bin);
+
+    this.bin = bin;
+    this.binPath = path;
+
+    logger.info(`ZipArchiveService use ${this.bin} from ${this.binPath}`);
+  }
+
+  private readonly bin: string;
+  private readonly binPath: string;
+
+  async extractAllFiles(file: string, output: string): Promise<string> {
+    const extractFile = new Promise<string>((res, rej) => {
+      logger.info(`Extracting file to ${output}`);
+      const stream = SevenZip.extractFull(file, output, { $bin: this.binPath });
+      stream.on('data', (data) => {
+        logger.info(`File ${data.file} ${data.status}`);
+      });
+      stream.on('end', () => {
+        logger.info('Extract completed');
+        res(output);
+      });
+      stream.on('error', (err) => {
+        logger.error(err, '7zip::decompressTemplateArchive');
+        rej(err);
+      });
+    });
+    return extractFile;
+  }
+}
