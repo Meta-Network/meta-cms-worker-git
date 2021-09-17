427967b6bd51f564e0e6dbc2b4de87d98a69a197
Garfield550 <openpgp@550.moe>
2021-09-17 08:36:23 +0000

    chore: remove unused

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,13 +1,5 @@
 import { MetaWorker } from '@metaio/worker-model';
 
-export type RemoveIndex<Q> = {
-  [key in keyof Q as string extends key
-    ? never
-    : key extends string
-    ? key
-    : never]: Q[key];
-};
-
 export type DownloadRepositoryArchiveReturn = {
   filePath: string;
   fileName: string;
