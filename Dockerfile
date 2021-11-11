e958e9751e24641e2c9c806019e1b5002c285776
Garfield550 <openpgp@550.moe>
2021-11-11 07:53:13 +0000

    feat: remove unused codes and configs

--- Dockerfile
+++ Dockerfile
@@ -7,21 +7,6 @@ RUN yarn run build
 RUN npm prune --production
 
 FROM ghcr.io/biscuittin/node:14-impish
-ENV SEVENZIP_VERSION=2103
-RUN ARCH= && dpkgArch="$(dpkg --print-architecture)" \
-  && case "${dpkgArch##*-}" in \
-    amd64) ARCH='x64';; \
-    arm64) ARCH='arm64';; \
-    i386) ARCH='x86';; \
-    *) echo "unsupported architecture"; exit 1 ;; \
-  esac \
-  && set -ex \
-  && curl -fLOSs "https://7-zip.org/a/7z$SEVENZIP_VERSION-linux-$ARCH.tar.xz" \
-  && mkdir -p /opt/7z \
-  && tar -xJf "7z$SEVENZIP_VERSION-linux-$ARCH.tar.xz" -C /opt/7z \
-  && rm "7z$SEVENZIP_VERSION-linux-$ARCH.tar.xz" \
-  && ln -s /opt/7z/7zz /usr/local/bin/7zz \
-  && 7zz
 WORKDIR /opt/MetaNetwork/Worker-Git
 COPY --from=builder /opt/MetaNetwork/Worker-Git/dist ./dist
 COPY --from=builder /opt/MetaNetwork/Worker-Git/node_modules ./node_modules
--- src/git/services/gitee.ts
+++ src/git/services/gitee.ts
@@ -1,11 +1,5 @@
 import { URL } from 'url';
 
-import { logger } from '../../logger';
-import {
-  BuildBasicInfoFromTemplateUrl,
-  BuildRemoteHttpUrlWithTokenReturn,
-} from '../../types';
-
 export class GiteeService {
   public static getServerUrl(): URL {
     return new URL('https://gitee.com');
@@ -23,40 +17,5 @@ export class GiteeService {
       'base64',
     );
     return basicCredential;
-  }
-
-  public static async buildRemoteGitUrl(
-    owner: string,
-    repo: string,
-  ): Promise<string> {
-    const remoteUrl = `https://gitee.com/${owner}/${repo}.git`;
-    logger.info(`Git remote url is: ${remoteUrl}`, this.constructor.name);
-    return remoteUrl;
-  }
-
-  public static async buildRemoteGitUrlWithToken(
-    token: string,
-    owner: string,
-    repo: string,
-  ): Promise<BuildRemoteHttpUrlWithTokenReturn> {
-    const originUrl = await this.buildRemoteGitUrl(owner, repo);
-    const remoteUrl = originUrl.replace(
-      'gitee.com',
-      `${owner}:${token}@gitee.com`,
-    );
-    return {
-      originUrl,
-      remoteUrl,
-    };
-  }
-
-  public static async buildBasicInfoFromGitUrl(
-    url: string,
-  ): Promise<BuildBasicInfoFromTemplateUrl> {
-    const info = url
-      .replace('https://gitee.com/', '')
-      .replace('.git', '')
-      .split('/');
-    return { owner: info[0], repo: info[1] };
   }
 }
